aed75e8063531e1a72899c6ffb3e4770ca134ac7
zhu-2002 <zenos0715@gmail.com>
2024-07-09 14:29:05 +0800

    添加自定义配置项实例

--- src/main/java/com/java/wiki/controller/BaseController.java
+++ src/main/java/com/java/wiki/controller/BaseController.java
@@ -1,18 +1,22 @@
 package com.java.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BaseController {
+
+    @Value("${author.name}")
+    private String name ;
     /**
      * /hello
-     * @return
+     * @return "hello"
      */
     @GetMapping("/hello")
     public String hello()
     {
-        return "hello";
+        return "hello ! "+name;
     }
 }
