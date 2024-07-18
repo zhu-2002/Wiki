99d3ef49b4047a44dbaadaa59bd6655e14d2d060
zhu-2002 <zenos0715@gmail.com>
2024-07-18 11:16:17 +0800

    7-3 新增用户登录名校验

--- src/main/java/com/java/wiki/exception/BusinessExceptionCode.java
+++ src/main/java/com/java/wiki/exception/BusinessExceptionCode.java
@@ -2,8 +2,7 @@ package com.java.wiki.exception;
 
 public enum BusinessExceptionCode {
 
-    USER_LOGIN_NAME_EXIST("登录名已存在"),
-    ;
+    USER_LOGIN_NAME_EXIST("登录名已存在") ;
 
     private String desc;
 
