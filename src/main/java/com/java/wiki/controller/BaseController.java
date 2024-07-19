7781c387b89bd0e890eee7dc637f5a80727525ba
zhu-2002 <zenos0715@gmail.com>
2024-07-19 15:25:14 +0800

    7-7 集成redis组件，登陆成功后将token存放到redis db0 数据库中

--- src/main/java/com/java/wiki/controller/BaseController.java
+++ src/main/java/com/java/wiki/controller/BaseController.java
@@ -1,22 +1,18 @@
 package com.java.wiki.controller;
 
+import com.java.wiki.util.RedisOperator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @CrossOrigin
 public class BaseController {
-    /**
-     * /hello
-     * @return "hello"
-     */
-    @GetMapping("/hello")
-    public String hello()
-    {
-        return "hello ! " ;
-    }
 
+    @Autowired
+    public RedisOperator redis;
+
+    public static final String REDIS_USER_TOKEN = "redis_user_token";
 
 }
--- src/main/java/com/java/wiki/controller/UserController.java
+++ src/main/java/com/java/wiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.java.wiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.java.wiki.req.UserLoginReq;
 import com.java.wiki.req.UserQueryReq;
 import com.java.wiki.req.UserResetPasswordReq;
@@ -9,18 +10,28 @@ import com.java.wiki.resp.UserLoginResp;
 import com.java.wiki.resp.UserQueryResp;
 import com.java.wiki.resp.PageResp;
 import com.java.wiki.service.UserService;
+import com.java.wiki.util.SnowFlake;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController extends BaseController{
 
-    @Resource
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
     private UserService userService;
+
+    @Autowired
+    private SnowFlake snowFlake;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req) {
@@ -55,6 +66,11 @@ public class UserController extends BaseController{
     public CommonResp login(@Valid @RequestBody UserLoginReq req) {
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+
+        Long token = snowFlake.nextId();
+        LOG.info("生成单点登录token：{}，并放入redis中", token);
+        userLoginResp.setToken(token.toString());
+        redis.set(REDIS_USER_TOKEN+":"+token, JSONObject.toJSONString(userLoginResp));
         resp.setContent(userLoginResp);
         return resp;
     }
