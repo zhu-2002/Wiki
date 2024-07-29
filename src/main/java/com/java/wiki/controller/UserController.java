10989eab6c5993003ca6bf08b941d8b9d5401455
zhu-2002 <zenos0715@gmail.com>
2024-07-29 12:32:30 +0800

    7-8 新增用户退出功能

--- src/main/java/com/java/wiki/controller/UserController.java
+++ src/main/java/com/java/wiki/controller/UserController.java
@@ -74,4 +74,12 @@ public class UserController extends BaseController{
         resp.setContent(userLoginResp);
         return resp;
     }
+
+    @GetMapping("/logout/{token}")
+    public CommonResp logout(@PathVariable String token) {
+        CommonResp resp = new CommonResp<>();
+        redis.del(REDIS_USER_TOKEN+":"+token);
+        LOG.info("从redis中删除token: {}", token);
+        return resp;
+    }
 }
